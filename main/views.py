5e7a5c717fbbb691ff569dd87350db4c7354854c
Anna Axmatova <ax.annushka@yandex.ru>
2025-03-13 18:04:44 +0300

    [UPDATE] - Tasks

--- main/views.py
+++ main/views.py
@@ -47,3 +47,42 @@ def painting(request):
 
 def porcelain(request):
     return render(request, 'porcelain.html')
+
+# Задачи 
+
+def task(request):
+    result_task1 = None
+    result_task2 = None
+
+    if request.method == 'POST':
+        if 'task1-submit' in request.POST:
+            # Первая задача
+            countries = request.POST.get('countries').split()
+            gdp_per_capita = list(map(float, request.POST.get('gdp_per_capita').split()))
+            threshold = float(request.POST.get('threshold'))
+
+            result_task1 = [
+                country for country, gdp in zip(countries, gdp_per_capita)
+                if gdp >= threshold
+            ]
+            if not result_task1:
+                result_task1 = "Нет стран, удовлетворяющих условию."
+
+        elif 'task2-submit' in request.POST:
+            # Вторая задача
+            year = int(request.POST.get('year'))
+
+            is_leap_year = (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+
+            century = (year - 1) // 100 + 1
+
+            result_task2 = {
+                'year': year,
+                'is_leap_year': is_leap_year,
+                'century': century,
+            }
+
+    return render(request, 'task.html', {
+        'result_task1': result_task1,
+        'result_task2': result_task2,
+    })
